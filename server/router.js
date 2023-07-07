e98afae20ebaa24fa9beb068293f69c4a109257a
annakucherenko1 <annakucherenko1997@gmail.com>
2023-07-07 15:23:59 +0200

    route changed

--- server/router.js
+++ server/router.js
@@ -14,7 +14,7 @@ router.post("/activities/join", userActivityController.joinParticipant);
 router.post("/activities/leave", userActivityController.leaveParticipant);
 router.get("/profile/:id", authMiddleware, usersController.getUserInfo);
 router.get("/activities", activityController.getActivities);
-router.get("/:id", activityController.getActivityInfo);
+router.get('/activity/:id', activityController.getActivityInfo);
 router.delete("/delete/:id", activityController.deleteActivity);
 router.put("/profile/edit/:id", usersController.editUser);
 router.put("/editactivity/:id", activityController.editActivity);
