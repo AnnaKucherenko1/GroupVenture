89b81786d6f151e5bd00fda90c93ca56232fa53c
annakucherenko1 <annakucherenko1997@gmail.com>
2023-07-07 15:01:08 +0200

    title changed

--- server/models/modelDB.js
+++ server/models/modelDB.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("GroupVenture", "postgres", "", {
+const sequelize = new Sequelize("postgres", "almun", "", {
   host: "localhost",
   dialect: "postgres",
   port: 5433,
