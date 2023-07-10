c4c4166b9ddb0c69b9f1a6cd2ac5cd80a847fbac
annakucherenko1 <annakucherenko1997@gmail.com>
2023-07-10 18:59:30 +0200

    Deploy FE and BE

--- server/models/modelDB.js
+++ server/models/modelDB.js
@@ -1,6 +1,9 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("postgres", "almun", "", {
+
+const {NODE_ENV, PROD_DB} = process.env;
+console.log("NODE_ENV & PROD_DB ==> ", NODE_ENV, PROD_DB)
+const sequelize = NODE_ENV==="production" ? new Sequelize(PROD_DB) :  new Sequelize("postgres", "almun", "", {
   host: "localhost",
   dialect: "postgres",
   port: 5432,
