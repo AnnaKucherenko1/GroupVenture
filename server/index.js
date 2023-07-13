81329f398e984bab315d58089ae9cea4ed44f1b9
annakucherenko1 <annakucherenko1997@gmail.com>
2023-07-13 20:49:57 +0200

    alter true added

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const sequelize = require("./models/modelDB");
 (async () => {
   try {
-    await sequelize.sync();
+    await sequelize.sync({alter: true});
     console.log("Connected to PostgreSQL DB!")
     const port = 3333;
     app.listen(port, () => {
